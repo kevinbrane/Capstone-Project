b8afbfa8a904c82a10bec6ba7591766e854be3ca
Kevin Bran <kevin_bran@epam.com>
2023-08-21 17:21:24 -0500

    Just tossed in some styles

--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -1,22 +1,40 @@
-import React, {forwardRef} from 'react';
-import './Skills.css'
+import React, { forwardRef } from "react";
+import "./Skills.css";
 
-const Skills:React.ForwardRefRenderFunction<HTMLDivElement> = (_, ref) => {
+const Skills: React.ForwardRefRenderFunction<HTMLDivElement> = (_, ref) => {
+  const skillData = [
+    { name: "HTML", percentage: "85%" },
+    { name: "CSS", percentage: "80%" },
+    { name: "Javascript", percentage: "50%" },
+    { name: "React", percentage: "70%" },
+    { name: "Typescript", percentage: "25%" },
+  ];
+
   return (
-    <div className='skills-container' ref={ref}>
+    <div className="skills-container" ref={ref}>
       <div>
-        <h1 className='skills-title'>Skills</h1>
+        <h1 className="skills-title">Skills</h1>
       </div>
-      <div>
-        <span>HTML</span>
-      </div>
-      <div className='scale-container'>
-        <img src="../src/assets/skills/scale.png" alt="" className='scale' />
-        <span>Begginer</span><span>Proficient</span><span>Expert</span><span>Master</span>
+      {skillData.map((skill,index) => (
+        <div className="skill-item" key={index}>
+          <span className="skill-name">{skill.name}</span>
+          <div className="skill-bar">
+            <div className="skill-level" style={{ width: skill.percentage }}></div>
+          </div>
+        </div>
+      ))}
+      <div className="scale-container">
+        <img src="../src/assets/skills/scale.png" alt="" className="scale" />
+        <div>
+          <span>Begginer</span>
+          <span>Proficient</span>
+          <span>Expert</span>
+          <span>Master</span>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const ForwardedSkills = forwardRef(Skills);
 export default ForwardedSkills;
