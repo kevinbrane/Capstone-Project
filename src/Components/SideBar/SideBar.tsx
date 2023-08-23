9d7d954d558aa75410d0d61d2159550870d32105
Kevin Bran <kevin_bran@epam.com>
2023-08-23 14:32:02 -0500

    Font awesome icons implemented

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser,faGraduationCap,faPencilAlt,faGem,faFolderOpen,faAddressBook,faComments} from '@fortawesome/free-solid-svg-icons';
 import PhotoBox from "../PhotoBox/PhotoBox";
 import Button from "../Button/Button";
 import "./SideBar.scss";
@@ -23,40 +25,37 @@ export default function SideBar({ scrollToRef, refs }: SideBarProps) {
       </div>
       <ul>
         <div onClick={() => scrollToRef(refs.aboutMe)}>
-          <img src="../src/assets/SideBar/Aboutme.png" alt="about me icon" />
+          <FontAwesomeIcon icon={faUser} />
           <li>About me</li>
         </div>
         <div onClick={() => scrollToRef(refs.education)}>
-          <img src="../src/assets/SideBar/Education.png" alt="education icon" />
+          <FontAwesomeIcon icon={faGraduationCap} />
           <li>Education</li>
         </div>
         <div onClick={() => scrollToRef(refs.experience)}>
-          <img
-            src="../src/assets/SideBar/Experience.png"
-            alt="experience icon"
-          />
+          <FontAwesomeIcon icon={faPencilAlt} />
           <li>Experience</li>
         </div>
         <div onClick={() => scrollToRef(refs.skills)}>
-          <img src="../src/assets/SideBar/Skills.png" alt="skills icon" />
+        <FontAwesomeIcon icon={faGem} />
           <li>Skills</li>
         </div>
         <div onClick={() => scrollToRef(refs.portfolio)}>
-          <img src="../src/assets/SideBar/Portfolio.png" alt="portfolio icon" />
+          <FontAwesomeIcon icon={faFolderOpen} />
           <li>Portfolio</li>
         </div>
         <div onClick={() => scrollToRef(refs.contacts)}>
-          <img src="../src/assets/SideBar/Contacts.png" alt="contacts icon" />
+          <FontAwesomeIcon icon={faAddressBook} />
           <li>Contacts</li>
         </div>
         <div onClick={() => scrollToRef(refs.feedbacks)}>
-          <img src="../src/assets/SideBar/Feedbacks.png" alt="feedback icon" />
+          <FontAwesomeIcon icon={faComments} />
           <li>Feedback</li>
         </div>
       </ul>
       <div className="goback-container">
-      <Button text="Go back" icon="../src/assets/SideBar/Vector.png" variant="goBack1" linkTo="/" />
-      <Button icon="../src/assets/SideBar/Go back_2.png" variant="goBack2" linkTo="/" />
+        <Button text="Go back" icon="../src/assets/SideBar/Vector.png" variant="goBack1" linkTo="/" />
+        <Button icon="../src/assets/SideBar/Go back_2.png" variant="goBack2" linkTo="/" />
       </div>
     </nav>
   );
